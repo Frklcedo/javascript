d2af3b2217557e356d6d3fd3bc36c8ae8432855d
Frklcedo <franklinmacedo5115@gmail.com>
2021-10-18 22:01:56 -0300

    arrays in Javascript

---
+++ netninjacourse/chapter2/arrays.js
@@ -0,0 +1,29 @@
+let ninjas = ['shaun','ryu','chun-li'];
+
+console.log(ninjas);
+
+console.log(ninjas[1]);
+
+// ninjas[1]= 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[3]);
+
+// let random = ['frk', 'cedo',20,21];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// let result = ninjas.join('_');
+
+// let result = ninjas.indexOf('chun-li');
+
+// let result = ninjas.concat(['ken','blanka']);
+
+let result = ninjas.push('ken');
+console.log(result);
+console.log(ninjas);
+result = ninjas.pop();
+console.log(result);
+console.log(ninjas);
