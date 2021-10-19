9963c77b1e1ceaf367b243eb33cfb6fc53e950a9
Frklcedo <franklinmacedo5115@gmail.com>
2021-10-19 17:37:00 -0300

    loose and strict comparisons

---
+++ netninjacourse/chapter2/loosenstrict.js
@@ -0,0 +1,15 @@
+let age = 20;
+
+// loose comparison (different types can still be equal)
+
+// console.log(age == 20);
+// console.log(age == '20');
+// console.log(age != 20);
+// console.log(age != '20');
+
+// strict comparison (different types cannot be equal)
+
+console.log(age===20);
+console.log(age==='20');
+console.log(age!==20);
+console.log(age!=='20');
