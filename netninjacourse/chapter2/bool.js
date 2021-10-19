4c644a1a81c5268ff60e2084e832fe7d99a047ba
Frklcedo <franklinmacedo5115@gmail.com>
2021-10-18 22:52:47 -0300

    booleans and comparisons

---
+++ netninjacourse/chapter2/bool.js
@@ -0,0 +1,33 @@
+//booleans and comparisons
+console.log(true,false, "true", "false");
+
+// methods can return booleans
+// let email = 'franklin@macemail.br';
+
+// let result = email.includes('@');
+// console.log(result);
+// result = email.includes('!');
+// console.log(result);
+
+// let names = ['frank','caesar','magnus'];
+// result = names.includes('frank');
+// console.log('names', result);
+// result = names.includes('carollus');
+// console.log('names', result);
+
+// comparison operators
+let age = 25;
+
+// console.log(age==25);
+// console.log(age==30);
+// console.log(age!=30);
+// console.log(age>20);
+// console.log(age<20);
+// console.log(age>=25);
+// console.log(age<=25);
+
+let nome = 'shaun';
+console.log(nome == 'shaun');
+console.log(nome == 'Shaun');
+console.log(nome > 'crystal'); // 's' comes later than 'c'
+console.log(nome > 'Shaun'); // lowercase letters are greater than uppercase letters
