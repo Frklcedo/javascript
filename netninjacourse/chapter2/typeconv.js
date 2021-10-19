dd3fe2690061f2426c21ed853350af136b58a5f5
Frklcedo <franklinmacedo5115@gmail.com>
2021-10-19 17:46:25 -0300

    type conversion

---
+++ netninjacourse/chapter2/typeconv.js
@@ -0,0 +1,16 @@
+// type conversion
+let score = '100';
+
+console.log(score+1);
+score = Number(score);
+console.log(score+1);
+console.log(typeof score);
+
+// let result = Number('Hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+// let result = Boolean('');
+
+console.log(result, typeof result);
