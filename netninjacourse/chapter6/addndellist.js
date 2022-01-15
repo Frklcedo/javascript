db4cd7bc38e3e21d13a7893386df0be6f0caeb7b
Frklcedo <franklinmacedo5115@gmail.com>
2022-01-15 16:21:00 -0300

    little to-do list

--- netninjacourse/chapter6/addndellist.js
+++ netninjacourse/chapter6/addndellist.js
@@ -1,23 +1,25 @@
-const list = document.querySelector('ul');
+const ul = document.querySelector('ul');
 // list.remove();
 
 const itens = document.querySelectorAll('li');
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 
-button.addEventListener('click',() =>{
-    if(input.value!=''){
-        const li = document.createElement('li');
-        li.textContent =input.value;
-        li.addEventListener('click',e =>{
-        e.target.remove();
-    });
-        list.append(li);
-        // list.prepend(li);
-    }
-});
 itens.forEach(item => {
     item.addEventListener('click',e =>{
         e.target.remove();
     });
+});
+
+button.addEventListener('click',() =>{
+    if(input.value!=''){
+        console.log(input.value);
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        li.addEventListener('click',e =>{
+            e.target.remove();
+        });
+    ul.append(li);
+    // ul.prepend(li);
+    }
 });
