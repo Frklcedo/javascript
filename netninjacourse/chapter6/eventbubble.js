32bf1432abbce834e93583a08dad294ae5a35b05
Frklcedo <franklinmacedo5115@gmail.com>
2022-01-16 21:02:48 -0300

    event bubbling and event delegation

--- netninjacourse/chapter6/eventbubble.js
+++ netninjacourse/chapter6/eventbubble.js
@@ -5,21 +5,30 @@ const itens = document.querySelectorAll('li');
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 
-itens.forEach(item => {
-    item.addEventListener('click',e =>{
-        e.target.remove();
-    });
-});
+// itens.forEach(item => {
+//     item.addEventListener('click',e =>{
+//         console.log('event in LI');
+//         e.stopPropagation(); // Stop event bubbling up
+//         e.target.remove();
+//     });
+// });
 
 button.addEventListener('click',() =>{
     if(input.value!=''){
         console.log(input.value);
         const li = document.createElement('li');
         li.textContent = input.value;
-        li.addEventListener('click',e =>{
-            e.target.remove();
-        });
         ul.append(li);
         // ul.prepend(li);
+    }
+});
+
+ul.addEventListener('click', e => {
+    // console.log('event in UL'); // event bubbling test
+
+    // event delegation 
+    console.log(e.target);
+    if(e.target.tagName === 'LI'){
+        e.target.remove();
     }
 });
