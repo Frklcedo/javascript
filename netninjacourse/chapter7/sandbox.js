8af872a9f7e242f8a2fa17c2434a70ea9b70a346
Frklcedo <franklinmacedo5115@gmail.com>
2022-01-19 19:08:00 -0300

    simple form validation

--- netninjacourse/chapter7/sandbox.js
+++ netninjacourse/chapter7/sandbox.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback-info');
 // const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
@@ -8,4 +9,27 @@ form.addEventListener('submit', e => {
     // reference obtained based on either id or name html attributes
     console.log(e.target.username.value);
     console.log(form.username.value);
+
+    // validation
+    const username = form.username.value;
+    const pattern = /^[a-zA-Z]{6,}$/;
+    const p = document.createElement('p');
+    p.textContent = '';
+    p.style.textAlign = 'center';
+    let result = pattern.test(username);
+    if(result){
+        form.username.classList.add('success');
+        form.username.classList.remove('error');
+        p.style.color = 'forestgreen';
+        p.textContent = 'This username is valid';
+    }
+    else{
+        form.username.classList.add('error');
+        form.username.classList.remove('success');
+        console.log(result);
+        p.style.color = 'crimson';
+        p.textContent = 'username must have only letters and be atleast 6 characters long';
+    }
+    feedback.innerHTML = p.outerHTML;
+    console.log(p.outerHTML);
 })
