a48d3ca24301cf30785c82ab7d83511e79a718d5
Frklcedo <franklinmacedo5115@gmail.com>
2022-01-25 01:03:00 -0300

    keyboard events

--- netninjacourse/chapter7/sandbox.js
+++ netninjacourse/chapter7/sandbox.js
@@ -1,3 +1,4 @@
+const unamePattern = /^[a-zA-Z]{6,}$/;
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback-info');
 // const username = document.querySelector('#username');
@@ -12,20 +13,17 @@ form.addEventListener('submit', e => {
 
     // validation
     const username = form.username.value;
-    const pattern = /^[a-zA-Z]{6,}$/;
     const p = document.createElement('p');
     p.textContent = '';
     p.style.textAlign = 'center';
-    let result = pattern.test(username);
+    let result = unamePattern.test(username);
     if(result){
-        form.username.classList.add('success');
-        form.username.classList.remove('error');
+        form.username.setAttribute('class', 'success');
         p.style.color = 'forestgreen';
         p.textContent = 'This username is valid';
     }
     else{
-        form.username.classList.add('error');
-        form.username.classList.remove('success');
+        form.username.setAttribute('class', 'error');
         console.log(result);
         p.style.color = 'crimson';
         p.textContent = 'username must have only letters and be atleast 6 characters long';
@@ -33,3 +31,25 @@ form.addEventListener('submit', e => {
     feedback.innerHTML = p.outerHTML;
     console.log(p.outerHTML);
 })
+
+// live feedback
+
+form.username.addEventListener('keyup',e => {
+    console.log(e.target.value, form.username.value);   // exactly the same thing
+
+    if(e.target.value != ''){
+        const result = unamePattern.test(e.target.value);
+        if(result){
+            form.username.style.color = 'forestgreen';
+            form.username.style.fontWeight = '';
+        }
+        else{
+            form.username.style.color = 'crimson';
+            form.username.style.fontWeight = 'bold';
+        }
+    }
+    else{
+            form.username.style.color = '';
+            form.username.style.fontWeight = '';
+    }
+});
