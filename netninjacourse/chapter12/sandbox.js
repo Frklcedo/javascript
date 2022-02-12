d2aa6d63a971899e4b8d5409a90711b95486f24f
Frklcedo <franklinmacedo5115@gmail.com>
2022-02-12 12:17:52 -0300

    callback functions and data response handling

---
+++ netninjacourse/chapter12/sandbox.js
@@ -0,0 +1,45 @@
+// console.log(1);
+// console.log(2);
+
+// setTimeout(() => {
+//     console.log(2.5);
+// },1000);
+
+// console.log(3);
+// console.log(4);
+
+// async function
+const getTodos = (callback) => {
+    
+    const request = new XMLHttpRequest();
+    
+    request.addEventListener('readystatechange', () => {
+        // console.log(request,request.readyState);
+        if(request.readyState === 4 && request.status === 200){
+            callback(undefined,request.responseText);
+        }
+        else if (request.readyState === 4){
+            callback('could not fetch the data',undefined);
+        }
+    });
+    
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    
+    request.send();
+
+};
+
+console.log(1);
+console.log(2);
+getTodos((error, data)=>{
+    console.log('callback fired');
+    // console.log(error,data);
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(data);
+    }
+});
+console.log(3);
+console.log(4);
