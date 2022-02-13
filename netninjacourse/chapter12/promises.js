64c1a0774c76159f749d9fb5b3ea3c5b93437734
Frklcedo <franklinmacedo5115@gmail.com>
2022-02-13 19:34:08 -0300

    intro to promises

---
+++ netninjacourse/chapter12/promises.js
@@ -0,0 +1,32 @@
+const getTodos = (resourse) => {
+
+    return new Promise( (resolve, reject) => {
+
+        const request = new XMLHttpRequest();
+        
+        request.addEventListener('readystatechange', () => {
+            // console.log(request,request.readyState);
+            if(request.readyState === 4 && request.status === 200){
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            }
+            else if (request.readyState === 4){
+                reject('error getting resource');
+            }
+        });
+        
+        request.open('GET',resourse);
+        // request.open('GET','todos.json'); // Testing own json file
+        
+        request.send();
+
+    });
+        
+
+};
+
+getTodos('todos/link.json').then(data => {
+    console.log('promise resolved: ', data);
+}).catch(error => {
+    console.log('promise rejected:', error);
+});
