ea8bc675f03b8a4c2a20ade6ec9abf95806e8628
Frklcedo <franklinmacedo5115@gmail.com>
2022-02-15 12:50:09 -0300

    update details and destructuring objects

--- netninjacourse/chapter13/weather_app/scripts/app.js
+++ netninjacourse/chapter13/weather_app/scripts/app.js
@@ -1,4 +1,33 @@
 const cityForm = document.querySelector('.city-location');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    // destructire properties
+    const { cityDets, weather } = data;
+
+    // Update details
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    // remove d-none
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+};
+
+// city and weather information
 
 const updateCity = async (city) => {
 
@@ -13,6 +42,7 @@ const updateCity = async (city) => {
     return { cityDets, weather };  // shorthand object notation
 };
 
+// submit form
 cityForm.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -23,6 +53,7 @@ cityForm.addEventListener('submit', e => {
     if(city != ''){
         updateCity(city).then(data => {
             console.log(data);
+            updateUI(data);
         }).catch(err => console.log(err));
     }
 
