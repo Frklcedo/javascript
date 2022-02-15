8ad23653ecd2417b4cb10176a90077079e3e0242
Frklcedo <franklinmacedo5115@gmail.com>
2022-02-15 12:24:58 -0300

    shorthand object notation

--- netninjacourse/chapter13/weather_app/scripts/app.js
+++ netninjacourse/chapter13/weather_app/scripts/app.js
@@ -5,10 +5,12 @@ const updateCity = async (city) => {
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    // return {
+    //     cityDets: cityDets,
+    //     weather: weather
+    // };
+    
+    return { cityDets, weather };  // shorthand object notation
 };
 
 cityForm.addEventListener('submit', e => {
